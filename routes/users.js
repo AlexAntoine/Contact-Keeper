8dc2c3aa06aae7ac5a8c871a4909994f8fcacc38
Alex Antoine <antoinealex1234@gmail.com>
2023-03-01 15:54:11 -0500

    added express validation middleware

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
+const User = require('../models/User');
 const {exampleMethod, exampleMethodTwo} = require('../controllers/user');
 
 const router = express.Router();
 
-router.route('/').post(exampleMethodTwo).get(exampleMethod);
+router.route('/').post([check('name',"please add name").not().isEmpty(),check('email', 'Please include a valid email').isEmail(),check('password','Please enter a password').isLength({min:6})],exampleMethodTwo,).get(exampleMethod);
 
 module.exports = router;
