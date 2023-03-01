5038483510c8b7d196a67470d39321782fe1083c
Alex Antoine <antoinealex1234@gmail.com>
2023-03-01 15:53:33 -0500

    added functionality for logging in

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 const asyncHandler = require('../middleware/async')
 
 // @desc
@@ -12,7 +15,34 @@ exports.exampleMethod = asyncHandler(async(req, res, next)=>{
 // @route
 // @access
 exports.exampleMethodTwo = asyncHandler(async(req, res, next)=>{
-    
+    const errors = validationResult(req);
+    // console.log('line 17: ', errors);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {name, email, password} = req.body;
+
+    var user = await User.findOne({email});
+    // console.log(user);
+    if(user){
+        return res.status(400).json({msg: 'User Already Exist'})
+    }
+
+    user = new User({
+        name,
+        email,
+        password
+    });
+
+    const salt = await bcrypt.genSalt(8);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User saved');
+
 });
 
 // @desc
