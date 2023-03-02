d5d3daaf6763d1674ad646916a4c8992c1b28b22
Alex Antoine <antoinealex1234@gmail.com>
2023-03-02 15:13:16 -0500

    added login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {exampleMethod} = require('../controllers/auth');
+const {check, validationResult} = require('express-validator');
+const {exampleMethod,login} = require('../controllers/auth');
 
 const router = express.Router();
 
-router.route('/').get(exampleMethod);
+router.route('/').get(exampleMethod).post([check('email','Please include a valid email').isEmail(), check('password','Password is required').exists()],login);
 
 module.exports = router;
