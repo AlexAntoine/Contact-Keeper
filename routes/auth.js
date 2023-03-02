75d4df9e6cc92504f96007cf3ed64058a97f464d
Alex Antoine <antoinealex1234@gmail.com>
2023-03-02 16:32:55 -0500

    added logged in user route and auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {check, validationResult} = require('express-validator');
-const {exampleMethod,login} = require('../controllers/auth');
+const auth = require('../middleware/auth')
+const {check} = require('express-validator');
+const {getLoggedInUser,login} = require('../controllers/auth');
 
 const router = express.Router();
 
-router.route('/').get(exampleMethod).post([check('email','Please include a valid email').isEmail(), check('password','Password is required').exists()],login);
+router.route('/').get(auth, getLoggedInUser).post([check('email','Please include a valid email').isEmail(), check('password','Password is required').exists()],login);
 
 module.exports = router;
