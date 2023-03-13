1e37c9333c3868a6f0964fe214e98a768e0c4a5d
Alex Antoine <antoinealex1234@gmail.com>
2023-03-13 13:39:10 -0400

    added navbar and navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({title, icon}) =>{
-    return (
-        <div className="navbar bg-primary">
-            <h1>
-                <i className={icon}/>{title}
-            </h1>
-        </div>
-    )
+ const Navbar = ({icon, title}) => {
+  return (
+    <div className='navbar bg-primary'>
+        <h1>
+            <i className={icon}/>{title}
+        </h1>
+        <ul>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                 <Link to='/about'>About</Link>
+            </li>
+        </ul>
+    </div>
+  )
 }
 
-Navbar.defaultProps = {
+Navbar.defaultProps ={
     title:"Contact Keeper",
-    icon:'fas fa-id-card-alt'
+    icon:"fas fa-id-card-alt"
 }
 
 export default Navbar;
