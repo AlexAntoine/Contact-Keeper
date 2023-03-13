5ec9fc8e011c1053d7910aa428c4f22716463260
Alex Antoine <antoinealex1234@gmail.com>
2023-03-13 14:40:49 -0400

    created contact state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -15,7 +15,59 @@ import {
 const ContactState = props =>{
 
     const initialState = {
-        contact:[]
-    }
+        contact:[
+            {
+                id:1,
+                name:'Alex Antoine',
+                email:'a@gmail.com',
+                phone:'111-111-111',
+                type:'Personsal'
+            },
+
+            {
+                id:2,
+                name:'Matt White',
+                email:'mwhite@gmail.com',
+                phone:'111-111-111',
+                type:'Personsal'
+            },
+
+            {
+                id:3,
+                name:'John Sinns',
+                email:'sinns@gmail.com',
+                phone:'111-111-111',
+                type:'Personsal'
+            }
+        ]
+    };
+
+    cosnt [state, dispatch] = useReducer(contactReducer, initialState);
+
+    //Actions
+
+    //Add Contact
+
+    //Delete Contact
+
+    //Set Current Contact
+
+    //Clear Contact
+
+    //Update Contact
+
+    //Filter Contacts
+
+    //Clear Filter
+
+    return (
+        <contactContext.Provider value={{
+            contacts:state.contacts
+        }}>
+            {props.childre}
+        </contactContext.Provider>
+    )
 
 }
+
+export default ContactState;
