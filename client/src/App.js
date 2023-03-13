60f36d46f14541dc951a83052459387e4ddb6986
Alex Antoine <antoinealex1234@gmail.com>
2023-03-13 13:40:10 -0400

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
+import Navbar from './components/layout/Navbar';
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
-import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Router>
+       <Fragment className="App">
+       <Navbar/>
+       <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+          </Routes>
+       </div>
+      </Fragment>
+    </Router>
   );
 }
 
